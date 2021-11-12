a7f5164dbb0632d2854b6c816ef51767463105d0
JyotiRanjan118 <jyotiranjan118@gmail.com>
2021-11-12 20:44:43 +0530

    File added and modified on Nov 12

--- src/Pages/SearchPage.java
+++ src/Pages/SearchPage.java
@@ -3,5 +3,6 @@ package Pages;
 public class SearchPage {
 	public static void main(String[] args) {
 		System.out.println("Hello LP");
+		System.out.println("New update on Nov 12");
 	}
 }
---
+++ src/tests/NewClass12.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class NewClass12 {
+		public static void main(String[] args) {
+			
+			System.out.println("New class added on 12 nov");
+		}
+}
