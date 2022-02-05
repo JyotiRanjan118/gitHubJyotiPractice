20117ad57c3ce024231334d01a2e7e8e5e32440a
JyotiRanjan118 <jyotiranjan118@gmail.com>
2022-02-05 19:24:45 +0530

    added and modified CartPage.java

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -4,8 +4,12 @@ public class CartPage {
 	
 	public static void main(String[] args) {
 		
-		String name="cart from dev2";
+		String name="cart";
 		System.out.println(name);
+		if(true) {
+			System.out.println("Cart is done");
+		}
+		
 	}
 
 }
