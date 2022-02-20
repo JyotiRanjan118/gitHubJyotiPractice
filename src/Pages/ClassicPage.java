b9f5c4f067fb59ee4dd2ecc43fdd3ee46fa93f5a
JyotiRanjan <53850272+JyotiRanjan118@users.noreply.github.com>
2022-02-20 11:33:59 +0530

    Create ClassicPage.java

---
+++ src/Pages/ClassicPage.java
@@ -0,0 +1,14 @@
+package Pages;
+
+public class ClassicPage {
+
+	public static void main(String[] args) {
+		int k=10;
+		int k1=20;
+		int div= n+1;
+		
+		System.out.println(div);
+    System.out.println("Bye");
+	}
+	
+}	
