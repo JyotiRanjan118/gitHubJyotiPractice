0901f4c8b7f69776f3704027f99a02a070771a54
JyotiRanjan118 <jyotiranjan118@gmail.com>
2022-02-20 11:51:07 +0530

    classicpage.java updated after merge conflict

--- src/Pages/ClassicPage.java
+++ src/Pages/ClassicPage.java
@@ -3,20 +3,14 @@ package Pages;
 public class ClassicPage {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		int n=10;
-		int n1=20;
-		int m = n+1;
-		
-		System.out.println(m);
-=======
+
 		int k=10;
 		int k1=20;
-		int div= n+1;
+		int div = k+k1;
 		
 		System.out.println(div);
-    System.out.println("Bye");
->>>>>>> b9f5c4f067fb59ee4dd2ecc43fdd3ee46fa93f5a
+        System.out.println("Bye");
+
 	}
 	
 }	
